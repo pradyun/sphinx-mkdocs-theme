992cd4dce5f6bca244455e82fca30de21dbdcf8a
Pradyun Gedam <pradyunsg@gmail.com>
2020-07-21 01:38:31 +0530

    We have a winning approach

--- src/sphinx_mkdocs_theme/__init__.py
+++ src/sphinx_mkdocs_theme/__init__.py
@@ -3,7 +3,7 @@
 
 import os
 
-from .bridge import EventHandler, MkDocsTemplateBridge
+from .bridge import MkDocsTemplateBridge
 from .builder import MkDocsBuilder
 
 __all__ = ["setup", "MkDocsTemplateBridge"]
@@ -12,14 +12,5 @@ __version__ = "0.0.1.dev0"
 
 # Entry point for the sphinx extension
 def setup(app):
-    # Approach 1
-    app.add_html_theme("mkdocs", os.path.abspath(os.path.dirname(__file__)))
     app.add_config_value("mkdocs_theme", default=None, rebuild="html")
-
-    handler = EventHandler()
-    app.connect("config-inited", handler.handle_config_inited)
-    app.connect("builder-inited", handler.handle_builder_inited)
-    app.connect("html-collect-pages", handler.handle_collect_pages)
-
-    # Approach 2
     app.add_builder(MkDocsBuilder)
